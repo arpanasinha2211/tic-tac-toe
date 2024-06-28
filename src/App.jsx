1fccb08cd1b4c3d94b2efc9117edbca176b007b8
Arpana Sinha <luckysinha022@gmail.com>
2024-06-28 22:30:23 +0530

    skeleton structure of game

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-
+import { Component } from "react";
+import Player from './components/player.jsx'
+import GameBoard from "./components/gamBoard.jsx";
 function App() {
-  
+
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player1" symbol="X" />
+          <Player initialName="Player2" symbol="O" />
+
+
+          {/* <li>
+            <span className="player">
+              <span className="player-name">Player2</span>
+              <span className="player-symbol">O</span>
+            </span>
+            <button>Edit</button>
+
+          </li> */}
+        </ol>
+        <GameBoard />
+      </div>
+
+    </main>
+  );
 }
 
 export default App
---
+++ src/components/player.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+export default function Player({ initialName, symbol }) {
+
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+
+        setIsEditing((editing) => !editing);
+    }
+
+    function handleChangeName(event) {
+        // console.log(event)
+        setPlayerName(event.target.value)
+    }
+    let editableplayerName = <span className="player-name">{playerName}</span>
+    let btnCaption = "Edit";
+    if (isEditing) {
+        editableplayerName = <input type="text" required value={playerName} onChange={handleChangeName} />;
+        btnCaption = "Save";
+    }
+    return (
+        <li>
+            <span className="player">
+                {editableplayerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            {/* Both method we can use to change the state of button cation */}
+            <button onClick={handleEditClick}>{btnCaption}</button>
+            {/* <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button> */}
+        </li>
+    );
+}
